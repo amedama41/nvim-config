157837ce6f8ccd068521abed0d9847bb18c67884
amedama41 <kamo.devel41@gmail.com>
2024-05-13 08:43:40 +0900

    Update config

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -24,13 +24,19 @@ return {
             mapping = {
                 ["<C-p>"] = cmp.mapping.select_prev_item(),
                 ["<C-n>"] = cmp.mapping.select_next_item(),
-                ["<C-l>"] = cmp.mapping.complete(),
+                ["<C-l>"] = function()
+                    if cmp.visible() then
+                        cmp.complete_common_string()
+                    else
+                        cmp.complete()
+                    end
+                end,
                 ["<C-b>"] = cmp.mapping.scroll_docs(-4),
                 ["<C-f>"] = cmp.mapping.scroll_docs(4),
                 ["<C-c>"] = cmp.mapping.close(),
                 ["<CR>"] = function(fallback)
                     -- https://github.com/hrsh7th/nvim-cmp/issues/1326
-                    if vim.fn.pumvisible() == 1 then
+                    if vim.fn.pumvisible() ~= 0 then
                         if vim.fn.complete_info({ "selected" }).selected == -1 then
                             feedkeys.call(keymap.t("<CR>"), "in")
                         else
