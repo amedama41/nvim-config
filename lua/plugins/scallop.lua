a625540219ba5b9e29c93c71d0c9f5c44d49c612
amedama41 <kamo.devel41@gmail.com>
2024-05-25 10:56:47 +0900

    Update config

--- lua/plugins/scallop.lua
+++ lua/plugins/scallop.lua
@@ -32,6 +32,31 @@ return {
                     concealcursor = "nvic",
                     foldmethod = "marker",
                 },
+                hooks = {
+                    init_terminal = function(buffer)
+                        vim.keymap.set("n", "a", function()
+                            require("scallop").open_edit()
+                        end, { buffer = buffer })
+                        vim.keymap.set("n", "i", function()
+                            require("scallop").open_edit()
+                        end, { buffer = buffer })
+                        vim.keymap.set("n", "<C-[>", function()
+                            require("scallop").close_terminal()
+                        end, { buffer = buffer })
+                        vim.keymap.set({ "n", "x" }, "<C-n>", function()
+                            require("scallop").jump_to_prompt("forward")
+                        end, { buffer = buffer })
+                        vim.keymap.set({ "n", "x" }, "<C-p>", function()
+                            require("scallop").jump_to_prompt("backward")
+                        end, { buffer = buffer })
+                        vim.keymap.set("n", "<C-y>", function()
+                            require("scallop").yank_from_prompt(true)
+                        end, { buffer = buffer })
+                        vim.keymap.set("n", "<C-^>", function()
+                            require("scallop").switch_terminal()
+                        end, { buffer = buffer })
+                    end,
+                },
             },
         })
         vim.keymap.set("n", "<C-k>", function()
@@ -40,6 +65,70 @@ return {
         vim.keymap.set("n", "g<C-k>", function()
             require("scallop").open_terminal()
         end, { noremap = true, silent = true })
+
+        local augroup = vim.api.nvim_create_augroup("vimrc-scallop-settings", { clear = true })
+        vim.api.nvim_create_autocmd("FileType", {
+            group = augroup,
+            pattern = { "bash.scallopedit" },
+            callback = function()
+                vim.lsp.start({
+                    name = "bashls_mod",
+                    -- cmd = { "/Users/Macbook/repos/bash-language-server/server/out/cli.js", "start" },
+                    cmd = { "bash-language-server-mod", "start" },
+                    root_dir = nil,
+                }, {
+                    reuse_client = function(client, config)
+                        return client.name == config.name
+                    end,
+                })
+
+                vim.keymap.set("n", "<C-[>", function()
+                    require("scallop").close_edit()
+                end, { buffer = true })
+                -- This mapping is needed for <C-g><C-c> mapping
+                vim.keymap.set("n", "<C-c>", function()
+                    require("scallop").send_to_terminal("<C-c>")
+                end, { buffer = true })
+                -- vim.keymap.set("i", "<C-c>", function()
+                --     require("scallop").close_terminal()
+                -- end, { buffer = true })
+                vim.keymap.set({ "n" }, "<C-^>", function()
+                    require("scallop").switch_terminal()
+                end, { buffer = true })
+                vim.keymap.set({ "n", "i" }, "<C-g><C-g>", function()
+                    require("scallop").scroll_to_bottom()
+                end, { buffer = true })
+                vim.keymap.set({ "n", "i" }, "<C-g><C-n>", function()
+                    require("scallop").jump_to_prompt("forward")
+                end, { buffer = true })
+                vim.keymap.set({ "n", "i" }, "<C-g><C-p>", function()
+                    require("scallop").jump_to_prompt("backward")
+                end, { buffer = true })
+                vim.keymap.set({ "n", "i" }, "<C-g><C-y>", function()
+                    require("scallop").yank_from_prompt(false)
+                end, { buffer = true })
+                vim.keymap.set({ "n", "i" }, "<C-g><C-^>", function()
+                    require("scallop").switch_terminal()
+                end, { buffer = true })
+                vim.keymap.set({ "n", "i" }, "<C-g>", function()
+                    local ok, char = pcall(vim.fn.getcharstr, 0)
+                    if not ok then
+                        vim.print(":" .. char .. ":")
+                        return
+                    end
+                    require("scallop").send_to_terminal(char)
+                end, { buffer = true })
+                vim.keymap.set("i", "<C-x><C-o>", function()
+                    require("cmp").complete({
+                        config = {
+                            sources = {
+                                { name = "nvim_lsp" },
+                            },
+                        },
+                    })
+                end, { buffer = true })
+            end,
+        })
 
         local cmp = require("cmp")
         local compare = require("cmp.config.compare")
@@ -119,94 +208,6 @@ return {
                     },
                 },
             },
-        })
-
-        local augroup = vim.api.nvim_create_augroup("vimrc-scallop-settings", { clear = true })
-        vim.api.nvim_create_autocmd("FileType", {
-            group = augroup,
-            pattern = { "scallop" },
-            callback = function()
-                vim.keymap.set("n", "a", function()
-                    require("scallop").open_edit()
-                end, { buffer = true })
-                vim.keymap.set("n", "i", function()
-                    require("scallop").open_edit()
-                end, { buffer = true })
-                vim.keymap.set("n", "<C-c>", function()
-                    require("scallop").close_terminal()
-                end, { buffer = true })
-                vim.keymap.set({ "n", "x" }, "<C-n>", function()
-                    require("scallop").jump_to_prompt("forward")
-                end, { buffer = true })
-                vim.keymap.set({ "n", "x" }, "<C-p>", function()
-                    require("scallop").jump_to_prompt("backward")
-                end, { buffer = true })
-                vim.keymap.set("n", "<C-y>", function()
-                    require("scallop").yank_from_prompt(true)
-                end, { buffer = true })
-                vim.keymap.set("n", "<C-^>", function()
-                    require("scallop").switch_terminal()
-                end, { buffer = true })
-            end,
-        })
-        vim.api.nvim_create_autocmd("FileType", {
-            group = augroup,
-            pattern = { "bash.scallopedit" },
-            callback = function()
-                vim.lsp.start({
-                    name = "bashls_mod",
-                    -- cmd = { "/Users/Macbook/repos/bash-language-server/server/out/cli.js", "start" },
-                    cmd = { "bash-language-server-mod", "start" },
-                    root_dir = nil,
-                }, {
-                    reuse_client = function(client, config)
-                        return client.name == config.name
-                    end,
-                })
-
-                -- This mapping is needed for <C-g><C-c> mapping
-                vim.keymap.set("n", "<C-c>", function()
-                    require("scallop").close_edit()
-                end, { buffer = true })
-                vim.keymap.set("i", "<C-c>", function()
-                    require("scallop").close_terminal()
-                end, { buffer = true })
-                vim.keymap.set({ "n" }, "<C-^>", function()
-                    require("scallop").switch_terminal()
-                end, { buffer = true })
-                vim.keymap.set({ "n", "i" }, "<C-g><C-g>", function()
-                    require("scallop").scroll_to_bottom()
-                end, { buffer = true })
-                vim.keymap.set({ "n", "i" }, "<C-g><C-n>", function()
-                    require("scallop").jump_to_prompt("forward")
-                end, { buffer = true })
-                vim.keymap.set({ "n", "i" }, "<C-g><C-p>", function()
-                    require("scallop").jump_to_prompt("backward")
-                end, { buffer = true })
-                vim.keymap.set({ "n", "i" }, "<C-g><C-y>", function()
-                    require("scallop").yank_from_prompt(false)
-                end, { buffer = true })
-                vim.keymap.set({ "n", "i" }, "<C-g><C-^>", function()
-                    require("scallop").switch_terminal()
-                end, { buffer = true })
-                vim.keymap.set({ "n", "i" }, "<C-g>", function()
-                    local ok, char = pcall(vim.fn.getcharstr, 0)
-                    if not ok then
-                        vim.print(":" .. char .. ":")
-                        return
-                    end
-                    require("scallop").send_to_terminal(char)
-                end, { buffer = true })
-                vim.keymap.set("i", "<C-x><C-o>", function()
-                    cmp.complete({
-                        config = {
-                            sources = {
-                                { name = "nvim_lsp" },
-                            },
-                        },
-                    })
-                end, { buffer = true })
-            end,
         })
     end,
 }
--- lua/plugins/telescope-project-tabs.lua
+++ lua/plugins/telescope-project-tabs.lua
@@ -17,8 +17,8 @@ return {
 
         local keymap_opts = { noremap = true, silent = true }
         vim.keymap.set("n", "<C-\\>p", project_tabs.switch_project, keymap_opts)
-        vim.keymap.set("n", "<C-\\>P", function()
-            project_tabs.switch_project({ only_opened = true })
+        vim.keymap.set("n", "<C-\\><C-p>", function()
+            project_tabs.switch_project({ only_opened = true, initial_mode = "normal" })
         end, keymap_opts)
     end,
 }
